b8d646d8501b17b85b2301fa77d74b475e8907a9
vickyvaf <vickyadi243@gmail.com>
2026-06-28 14:43:06 +0700

    feat: add database migrations, factories, and seeders

---
+++ database/migrations/2026_06_28_060129_create_answers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('quiz_attempt_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('question_id')->constrained()->cascadeOnDelete();
+            $table->text('answer_text');
+            $table->boolean('is_correct')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
