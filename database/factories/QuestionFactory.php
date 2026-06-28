e2a5f66e7cc64d97929e90eb7ebfa2837e585000
vickyvaf <vickyadi243@gmail.com>
2026-06-28 18:00:08 +0700

    factories: create QuestionFactory and QuizFactory

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Question;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Question>
+ */
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
